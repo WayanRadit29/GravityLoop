89a43fa7e510e517f8db36710248f1d95db86a1b
WayanRadit29 <radityaputraa1122@gmail.com>
2025-11-20 23:09:25 +0700

    Setup Src Folder dan Isi bebera kode

---
+++ src/planet.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import *
+
+
+class Planet:
+    def __init__(self, x, y, radius, gravity_range):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+        self.gravity_range = gravity_range
+        self.gravity_strength = 0.6     # bisa dituning nanti
+
+        self.color = (120, 150, 255)
+        self.range_color = (80, 80, 120)
+
+    def render(self, screen):
+        # render area gravitasi (sementara sebagai lingkaran transparan)
+        pygame.draw.circle(screen, self.range_color, (self.x, self.y), self.gravity_range, 1)
+
+        # render planet
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
---
+++ src/settings.py
@@ -0,0 +1,7 @@
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+COLORS = {
+    "bg": (5, 5, 15),
+}
